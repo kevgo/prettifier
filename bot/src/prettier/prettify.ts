e1fb4b807983b099ca40b3909f5624022f162757
Kevin Goslar <kevin.goslar@gmail.com>
2020-11-02 11:52:37 -0600

    Simplify prettify code

--- bot/src/prettier/prettify.ts
+++ bot/src/prettier/prettify.ts
@@ -2,10 +2,8 @@ import prettier from "prettier"
 
 /** Formats the given content for the given file using the given Prettier configuration. */
 export function prettify(text: string, filename: string, prettierConfig: prettier.Options): string {
-  const options = { ...prettierConfig }
-  options.filepath = filename
   try {
-    return prettier.format(text, options)
+    return prettier.format(text, { ...prettierConfig, filepath: filename })
   } catch (e) {
     return text
   }
